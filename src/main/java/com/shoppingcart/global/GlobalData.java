72b5008853fba339b33c38540d804001e9e4bc54
krishnaveer <krishnaveer.chahar_cs19@gla.ac.in>
2021-11-25 03:34:42 +0530

    Created GlobalData.java

---
+++ src/main/java/com/shoppingcart/global/GlobalData.java
@@ -0,0 +1,15 @@
+package com.shoppingcart.global;
+
+import com.shoppingcart.model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GlobalData {
+
+    public static List<Product> cart;
+
+    static{
+        cart = new ArrayList<>();
+    }
+}

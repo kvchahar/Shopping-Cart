01f981495688074ebd1b0ab86a0c8de9075e887f
krishnaveer <krishnaveer.chahar_cs19@gla.ac.in>
2021-11-25 03:27:53 +0530

    Created CategoryRepository.java

---
+++ src/main/java/com/shoppingcart/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.shoppingcart.repositories;
+
+
+import com.shoppingcart.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category,Integer> {
+
+}
